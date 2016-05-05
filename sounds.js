66ab8aae95ffe98725be63e1624d39ec95387183
Alex Garcia <agarcia@gmrmarketing.com>
2016-05-05 20:50:16 +0100

    More sound files

--- sounds.js
+++ sounds.js
@@ -1,5 +1,5 @@
 var Sound = require('node-aplay');
-var files = ['roll', 'getup', 'ball', 'downinfront', 'freebies', 'frontrow'];
+var files = ['roll', 'getup', 'ball', 'downinfront', 'freebies', 'frontrow', '8', '9', '10', '11'];
 var index = 0;
 
 exports.playSound = function () {
